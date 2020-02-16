c9f5199a5a8710bc8b3155ec147a7e0c24a6071c
gy61628388 <310883101@qq.com>
2020-02-16 08:12:16 +0800

    增加测试

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,24 @@
-import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+// import { shallowMount } from "@vue/test-utils";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
-describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+// describe("HelloWorld.vue", () => {
+//   it("renders props.msg when passed", () => {
+//     const msg = "new message";
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
+function add(num1,num2){
+  return num1 + num2
+}
+
+describe('加法测试',()=>{
+  it('数字相加',()=>{
+    expect(add(1,2)).toBe(3)
+  })
+  it('数字相加',()=>{
+    expect(add(2,4)).toBe(6)
   })
 })
